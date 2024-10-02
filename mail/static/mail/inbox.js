5065dd5b04979b998e7828678187e67502cb0de2
david-borja <davidborjamartinez@gmail.com>
2024-10-02 23:37:12 +0200

    feat(): global refactor and email detail WIP

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,33 +1,74 @@
+// GLOBAL QUERY SELECTORS
+
+let $emailsView
+let $composeView
+let $emailDetail
+let $composeRecipients
+let $composeSubject
+let $composeBody
+
+const $ = (element) => document.querySelector(element)
+// TO DO: create a utility to set visibility
+
 document.addEventListener('DOMContentLoaded', function () {
   // Use buttons to toggle between views
-  document
-    .querySelector('#inbox')
-    .addEventListener('click', () => load_mailbox('inbox'))
-  document
-    .querySelector('#sent')
-    .addEventListener('click', () => load_mailbox('sent'))
-  document
-    .querySelector('#archived')
-    .addEventListener('click', () => load_mailbox('archive'))
-  document.querySelector('#compose').addEventListener('click', compose_email)
+  $('#inbox').addEventListener('click', () => load_mailbox('inbox'))
+  $('#sent').addEventListener('click', () => load_mailbox('sent'))
+  $('#archived').addEventListener('click', () => load_mailbox('archive'))
+  $('#compose').addEventListener('click', compose_email)
+
+  $emailsView = $('#emails-view')
+  $composeView = $('#compose-view')
+  $emailDetail = $('#email-detail')
+  $composeRecipients = $('#compose-recipients')
+  $composeSubject = $('#compose-subject')
+  $composeBody = $('#compose-body')
 
   // By default, load the inbox
   load_mailbox('inbox')
 })
 
-const handleSubmit = () => {
-  const recipients = document.querySelector('#compose-recipients').value
-  const subject = document.querySelector('#compose-subject').value
-  const body = document.querySelector('#compose-body').value
+// API //
 
-  fetch('/emails', {
+const getEmailById = (id) => {
+  return fetch(`/emails/${id}`)
+    .then((response) => response.json())
+    .then((email) => email)
+}
+
+const getEmailsByMailbox = (mailbox) => {
+  return fetch(`/emails/${mailbox}`)
+    .then((response) => response.json())
+    .then((emails) => emails)
+}
+
+const postEmail = (body) => {
+  return fetch('/emails', {
     method: 'POST',
-    body: JSON.stringify({ recipients, subject, body }),
+    body: JSON.stringify(body),
   })
     .then((response) => response.json())
-    .then((result) => load_mailbox('sent'))
+    .then((result) => result)
+}
+
+// HANDLERS //
+
+const handleSubmit = () => {
+  const recipients = $composeRecipients.value
+  const subject = $composeSubject.value
+  const body = $composeBody.value
+
+  postEmail({ recipients, subject, body }).then(() => {
+    load_mailbox('sent')
+  })
   return false
 }
+
+const handleEmailClick = (email, mailbox) => {
+  renderEmailDetail(email, $emailDetail, mailbox)
+}
+
+// RENDERERS
 
 const renderEmails = (emails, $node, mailbox) => {
   const isInbox = mailbox === 'inbox'
@@ -35,43 +76,51 @@ const renderEmails = (emails, $node, mailbox) => {
   $node.append($emailsFeed)
 
   emails.forEach((email) => {
-    const $div = document.createElement('li')
+    // TO DO: change clickable div for a button
+    const $div = document.createElement('div')
     $div.className = 'email'
     const emailAddress = isInbox ? email.sender : `To: ${email.recipients[0]}`
     $div.innerHTML = `<div><span>${emailAddress}</span><span>${email.subject}</span>
     </div><span>${email.timestamp}</span>`
+    $div.onclick = () => handleEmailClick(email)
     $emailsFeed.append($div)
   })
 }
 
+const renderEmailDetail = (email, $node) => {
+  $emailsView.style.display = 'none'
+  $node.style.display = 'block'
+  $node.innerHTML = JSON.stringify(email)
+}
+
+// MAIN FUNCTIONS //
+
 function compose_email() {
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none'
-  document.querySelector('#compose-view').style.display = 'block'
+  $emailsView.style.display = 'none'
+  $emailDetail.style.display = 'none'
+  $composeView.style.display = 'block'
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = ''
-  document.querySelector('#compose-subject').value = ''
-  document.querySelector('#compose-body').value = ''
+  $composeRecipients.value = ''
+  $composeSubject.value = ''
+  $composeBody.value = ''
 
-  document.querySelector('#compose-form').onsubmit = handleSubmit
+  $('#compose-form').onsubmit = handleSubmit
 }
 
 function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
-  const $emailsView = document.querySelector('#emails-view')
-  const $composeView = document.querySelector('#compose-view')
   $emailsView.style.display = 'block'
   $composeView.style.display = 'none'
+  $emailDetail.style.display = 'none'
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${
+  $emailsView.innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`
 
-  fetch(`/emails/${mailbox}`)
-    .then((response) => response.json())
-    .then((emails) => {
-      renderEmails(emails, $emailsView, mailbox)
-    })
+  getEmailsByMailbox(mailbox).then((emails) => {
+    renderEmails(emails, $emailsView, mailbox)
+  })
 }
